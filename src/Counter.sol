0485df9bfbd83d9f4ef04cffbb712fcdadcb58c3
zeroknots <zeroknots@protonmail.com>
2023-03-23 15:03:58 +0700

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ZeroknotsPFP is ERC721 {
     uint256 public number;
